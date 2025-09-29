cf930ad23b2907ed3956305ef264240f60c5df6c
BeatricePapi-STUDENT <G00461762@atu.ie>
2025-09-29 10:11:10 +0100

    Added new method called power

--- src/main/java/ie/atu/week2/Calculator.java
+++ src/main/java/ie/atu/week2/Calculator.java
@@ -4,8 +4,12 @@ public class Calculator {
     public Calculator() {
     }
 
-    public int divide(int firstNumber, int secondNumber) {
-        return firstNumber / secondNumber;
+    public int power(int base, int exponent) {
+        int result = 1;       // Step 1: start with 1
+        for (int i = 1; i <= exponent; i++) {
+            // Step 2: repeat exponent times
+            result *= base;     //multiply result by base each time
+        }
+        return result;      // Step 3: give back the result
     }
-    // You will add subtract / multiply / divide later
 }
