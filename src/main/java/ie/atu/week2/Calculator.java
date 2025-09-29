486b6e710f0dca903877e87c1a77de29e2ba79ba
BeatricePapi-STUDENT <G00461762@atu.ie>
2025-09-29 09:34:52 +0100

    Added new method called add

---
+++ src/main/java/ie/atu/week2/Calculator.java
@@ -0,0 +1,11 @@
+package ie.atu.week2;
+
+public class Calculator {
+    public Calculator() {
+    }
+
+    public int add(int firstNumber, int secondNumber) {
+        return firstNumber + secondNumber;
+    }
+    // You will add subtract / multiply / divide later
+}
