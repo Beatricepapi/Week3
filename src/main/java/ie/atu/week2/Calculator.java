c9ae039a8ea2c84feb5200792293839003839186
BeatricePapi-STUDENT <G00461762@atu.ie>
2025-09-29 09:55:12 +0100

    Added new method called multiply

--- src/main/java/ie/atu/week2/Calculator.java
+++ src/main/java/ie/atu/week2/Calculator.java
@@ -4,8 +4,8 @@ public class Calculator {
     public Calculator() {
     }
 
-    public int subtract(int firstNumber, int secondNumber) {
-        return firstNumber - secondNumber;
+    public int multiply(int firstNumber, int secondNumber) {
+        return firstNumber * secondNumber;
     }
     // You will add subtract / multiply / divide later
 }
